7ea441c14b11e117457060de6269f42b7616785f
yarik <plehanov.kzn2@gmail.com>
2025-03-05 23:52:40 +0300

    added unit tests

--- MyStack/Stack/MyStackTests.cs
+++ MyStack/Stack/MyStackTests.cs
@@ -1,6 +1,35 @@
+using Xunit;
+
 namespace MyStack.Stack;
 
 public class MyStackTests
 {
-    
+    [Fact]
+    public void PushTest()
+    {
+        var stack = new MyStack<int>();
+        stack.Push(10);
+        Assert.Equal(10, stack.Peek());
+        Assert.Equal(1, stack.Count);
+    }
+
+    [Fact]
+    public void PopTest()
+    {
+        var stack = new MyStack<int>();
+        stack.Push(10);
+        stack.Push(20);
+        
+        var popped = stack.Pop();
+        
+        Assert.Equal(20, popped);
+        Assert.Equal(1, stack.Count);
+    }
+
+    [Fact]
+    public void PopThrowsExceptionOnEmptyStack()
+    {
+        var stack = new MyStack<int>();
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
 }
